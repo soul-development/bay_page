6360fe1c19c4fb6d1b763658f62d21f8988d9a73
bmspd <my_spid@mail.ru>
2025-02-05 19:14:42 +0100

    feat(page): add intersection observer

--- js/main.js
+++ js/main.js
@@ -1,16 +1,46 @@
 const switcher = document.querySelector(".switch input");
-const switchPrice = document.querySelector('.switch-price')
-const switchMode = document.querySelector('.switch-mode')
+const switchPrice = document.querySelector(".switch-price");
+const switchMode = document.querySelector(".switch-mode");
 if (switcher) {
   switcher.addEventListener("change", (e) => {
     console.log(e.target.checked, "privet");
 
     if (e.target.checked) {
-      switchPrice.innerHTML = '990₽'
-      switchMode.innerHTML = '<span class="unlimited-symb">∞</span> на месяц'
+      switchPrice.innerHTML = "990₽";
+      switchMode.innerHTML = '<span class="unlimited-symb">∞</span> на месяц';
     } else {
-      switchPrice.innerHTML = '490₽'
-      switchMode.innerHTML = '200 постов'
+      switchPrice.innerHTML = "490₽";
+      switchMode.innerHTML = "200 постов";
     }
   });
+}
+
+const appearanceEls = Array.from(
+  document.querySelectorAll(".appearance-animation")
+).map((el) => ({ el, className: "appearance" }));
+const fadeInEls = Array.from(
+  document.querySelectorAll(".fade-in-animation")
+).map((el) => ({ el, className: "fade-in" }));
+const fadeInXEls = Array.from(
+  document.querySelectorAll(".fade-in-x-animation")
+).map((el) => ({ el, className: "fade-in-x" }));
+
+const sectionContents = [...appearanceEls, ...fadeInEls, ...fadeInXEls];
+if (sectionContents.length) {
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const section = sectionContents.find((s) => s.el === entry.target);
+          console.log(section);
+          if (section) {
+            entry.target.classList.add(section.className);
+            observer.unobserve(entry.target);
+          }
+        }
+      });
+    },
+    { threshold: 0.2 }
+  );
+  sectionContents.forEach((section) => observer.observe(section.el));
 }
